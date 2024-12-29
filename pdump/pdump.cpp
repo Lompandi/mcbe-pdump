430e924b9c516a9b822f75ea41ed1c6ae5856aef
Lompandi <humingshun33@gmail.com>
2024-12-29 22:01:30 +0800

    Add args option

--- pdump/pdump.cpp
+++ pdump/pdump.cpp
@@ -10,6 +10,12 @@ int main(int argc, char** argv) {
         std::print("You must specify a executable path to bedrock server!\n");
         return 0;
     }
+    pdm::g_CmdOpts.add_option("--dump", "true");
+    pdm::g_CmdOpts.add_option("--dump-max", "1024");
+    pdm::g_CmdOpts.add_option("--dump-decode", "false");
+
+    pdm::g_CmdOpts.add_option("--verbose", "false");
+    pdm::g_CmdOpts.add_option("--output", "");
 
     //"D:\\bedrock-server-1.21.51.02\\bedrock_server.exe"
     pdm::g_ExecutablePath = argv[1];
